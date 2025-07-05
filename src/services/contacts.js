197578718c6a26b19de2e4739a07a513f6ca9262
Nilay Türker <nilaytr89qgmail.com>
2025-07-05 13:34:50 +0300

    hw-4

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,42 @@
 import { ContactsCollection } from '../db/models/Contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+export const getAllContacts = async ({
+    page = 1,
+    perPage = 10,
+    sortOrder = SORT_ORDER.ASC,
+    sortBy = '_id',
+    filter = {},
+}) => {
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+
+    const contactQuery = ContactsCollection.find();
+
+    if (filter.contactType) {
+        contactQuery.where('contactType').equals(filter.contactType);
+    }
+
+    if (typeof filter.isFavourite === 'boolean') {
+        contactQuery.where('isFavourite').equals(filter.isFavourite);
+    }
+
+    const [contactCount, contacts] = await Promise.all([
+        ContactsCollection.find().merge(contactQuery).countDocuments(),
+        contactQuery
+            .skip(skip)
+            .limit(limit)
+            .sort({ [sortBy]: sortOrder })
+            .exec(),
+    ]);
+
+    const paginationData = calculatePaginationData(contactCount, perPage, page);
+
+    return {
+        data: contacts,
+        ...paginationData,
+    };
 };
 
 export const getContactById = async (contactId) => {
---
+++ src/utils/parseFilterParams.js
@@ -0,0 +1,54 @@
+const parseContactType = (contactType) => {
+    const isString = typeof contactType === 'string';
+    if (!isString) return;
+    const isContactType = (contactType) => ['work', 'home', 'personal'].includes(contactType);
+
+    if (isContactType(contactType)) return contactType;
+};
+
+const parseIsFavourite = (isFavourite) => {
+    const isString = typeof isFavourite === 'string';
+    if(!isString) return;
+    return isFavourite;
+};
+
+export const parseFilterParams = (query) => {
+    const { contactType, isFavourite } = query;
+
+    const parsedContactType = parseContactType(contactType);
+    const parsedIsFavourite = parseIsFavourite(isFavourite);
+
+    return {
+        contactType: parsedContactType,
+        isFavourite: parsedIsFavourite,
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
