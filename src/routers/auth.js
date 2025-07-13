8d0b6cdc3d6b278b1280c1ba82734337a2abaefd
Nilay Türker <nilaytr89qgmail.com>
2025-07-13 15:21:05 +0300

    check

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,7 +8,6 @@ import {
     refreshUserSessionController,
 } from '../controllers/auth.js';
 import validateBody from '../middlewares/validateBody.js';
-import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
@@ -24,8 +23,8 @@ router.post(
     ctrlWrapper(loginUserController)
 );
 
-router.post('/logout', authenticate, ctrlWrapper(logoutUserController));
-router.post('/refresh', authenticate, ctrlWrapper(refreshUserSessionController));
+router.post('/logout', ctrlWrapper(logoutUserController));
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default router;
 
